0b8b22fcdf18e7d2f17a24825755ddf0506fe55a
bradybyrd-mongodb <brady.byrd@mongodb.com>
2020-09-08 08:48:18 -0400

    adding python script to get full serverStatus - refactor process2 to have discrete files

---
+++ monitor/full_monitor.py
@@ -0,0 +1,176 @@
+# Python Atlas API
+import sys
+import csv
+from collections import OrderedDict
+from collections import defaultdict
+import json
+import logging
+import datetime
+import random
+import uuid
+import os
+import io
+import subprocess
+import time
+import re
+import multiprocessing
+import faker
+import pprint
+import getopt
+from contextlib import redirect_stdout
+from bson.objectid import ObjectId
+#import bbhelper as bb
+from datetime import datetime
+from pymongo import MongoClient
+settings = {}
+settings_file = "monitor_settings.json"
+last_vals = {}
+last_doc = {}
+first_time = True
+
+def read_json(json_file):
+    result = {}
+    with open(json_file) as jsonfile:
+        result = json.load(jsonfile)
+    return result
+
+def process_args(arglist):
+    args = {}
+    for arg in arglist:
+        pair = arg.split("=")
+        if len(pair) == 2:
+            args[pair[0].strip()] = pair[1].strip()
+        else:
+            args[arg] = ""
+    return args
+
+def fix_vals(curdoc):
+    curdoc["conn"] = int(curdoc["conn"])
+    curdoc["flushes"] = int(curdoc["flushes"])
+    curdoc["getmore"] = int(curdoc["getmore"])
+    #curdoc["extra_infoSystem_time_us"] = int(curdoc.pop("extra_info.system_time_us"))
+    #del curdoc["extra_info.system_time_us"]
+    #curdoc["extra_infoUser_time_us"] = int(curdoc["extra_info.user_time_us"])
+    #del curdoc["extra_info.user_time_us"]
+    cur_trans = int(curdoc["transactions.totalCommitted"])
+    if "host" in curdoc:
+        print(f'Host: {curdoc["host"]}')
+        host = curdoc["host"]
+        diff = 0
+        if host in last_vals:
+            diff = cur_trans - last_vals[host]
+        curdoc["transactionsNet"] = diff
+    
+    curdoc["transactionsTotalCommitted"] = cur_trans
+    del curdoc["transactions.totalCommitted"]
+    #curdoc["insert"] = int(curdoc["insert"].replace("*",""))
+    #curdoc["update"] = int(curdoc["update"].replace("*",""))
+    #curdoc["delete"] = int(curdoc["delete"].replace("*",""))
+    curdoc["query"] = int(curdoc["query"].replace("*",""))
+    curdoc["version"] = "1.0"
+    return(curdoc)
+
+def clean_key(key):
+    return key.replace(":","-").replace(".","_").replace("@","").replace(",","")
+
+def run_mongostat():
+    host = settings["source"]["host"]
+    port = settings["source"]["port"]
+    username = settings["source"]["username"]
+    password = settings["source"]["password"]
+    numrows = settings["mongostat_batch_size"]
+    options = settings["mongostat_options"]
+    cmd = ["mongostat", "--host", f'"{host}"', "--port", f'"{port}"', "--username", f'"{username}"', "--password", f'"{password}"', "--authenticationDatabase", "admin", "--ssl", "--discover", f'--rowcount={numrows}', "--json", f'-O="{options}"']
+    result = run_shell(cmd)
+    fixed = result.stdout
+    fixed = fixed.decode()
+    fixed = fixed.replace("\n{",",\n{")
+    fixed = f'[{fixed}]'
+    #print("Processed ------------------------------------")
+    #print(fixed)
+    stats = json.loads(fixed)
+    return stats
+
+def db_stats(dbclient = "none"):
+    global first_time
+    if dbclient == "none":
+        user = settings["source"]["username"]
+        pwd = settings["source"]["password"]
+        s_uri = settings["source"]["uri"]
+        s_uri = s_uri.replace("//", f'//{user}:{pwd}@')
+        print(f'Source: {s_uri}')
+        dbclient = MongoClient(s_uri)
+    iters = settings["mongostat_batch_size"]
+    batch = []
+    for inc in range(iters):
+        res = dbclient.admin.command("serverStatus")
+        #print("--- Status Output ---")
+        #print(res)
+        print(".", end="", flush=True)
+        del res["transportSecurity"]
+        del res["metrics"]["aggStageCounters"]
+        del res["$clusterTime"]
+        if not first_time: #"ok" in last_doc:
+            res["opcounters"]["insertDelta"] = res["opcounters"]["insert"] - last_doc["opcounters"]["insert"]
+            res["opcounters"]["updateDelta"] = res["opcounters"]["update"] - last_doc["opcounters"]["update"]
+            res["opcounters"]["deleteDelta"] = res["opcounters"]["delete"] - last_doc["opcounters"]["delete"]
+            res["opcounters"]["queryDelta"] = res["opcounters"]["query"] - last_doc["opcounters"]["query"]
+            res["opcounters"]["getmoreDelta"] = res["opcounters"]["getmore"] - last_doc["opcounters"]["getmore"]
+            res["opcounters"]["commandDelta"] = res["opcounters"]["command"] - last_doc["opcounters"]["command"]
+            res["transactions"]["totalCommittedDelta"] = res["transactions"]["totalCommitted"] - last_doc["transactions"]["totalCommitted"]
+            res["transactions"]["totalStartedDelta"] = res["transactions"]["totalStarted"] - last_doc["transactions"]["totalStarted"]
+            res["transactions"]["totalAbortedDelta"] = res["transactions"]["totalAborted"] - last_doc["transactions"]["totalAborted"]
+        else:
+            res["opcounters"]["insertDelta"] = 0
+            res["opcounters"]["updateDelta"] = 0
+            res["opcounters"]["deleteDelta"] = 0
+            res["opcounters"]["queryDelta"] = 0
+            res["opcounters"]["getmoreDelta"] = 0
+            res["opcounters"]["commandDelta"] = 0
+            res["transactions"]["totalCommittedDelta"] = 0
+            res["transactions"]["totalStartedDelta"] = 0
+            res["transactions"]["totalAbortedDelta"] = 0
+            first_time = False    
+        batch.append(res)
+        last_doc = res
+        time.sleep(1)
+    print(" done")
+    return batch
+
+    
+def run_shell(cmd = ["ls", "-l"]):
+    result = subprocess.run(cmd, capture_output=True)
+    print("The exit code was: %d" % result.returncode)
+    print("#--------------- STDOUT ---------------#")
+    print(result.stdout)
+    if result.stderr:
+        print("#--------------- STDERR ---------------#")
+        print(result.stderr)
+    return result
+
+
+#-----------------------------------------------------------#
+#------------------------  MAIN ----------------------------#
+if __name__ == "__main__":
+    ARGS = process_args(sys.argv)
+    settings = read_json(settings_file)
+    uri = settings["logger"]["uri"]
+    username = settings["logger"]["username"]
+    password = settings["logger"]["password"]
+    database = settings["logger"]["database"]
+    batches = settings["batches"]
+    testname = ARGS["testname"]
+    #shards = settings["source"]["shards"]
+    uri = uri.replace("//", f'//{username}:{password}@')
+    client = MongoClient(uri) #&w=majority
+    mdb = client[database]
+    print(f'Opening {uri}')
+    print(f'Performing {batches} batches')
+    for iter in range(batches):
+        print(f'Gathering stats {settings["mongostat_batch_size"]} times using 1 second interval')     
+        result = db_stats()
+        print(f'Batch {len(result)} items to do')
+        mdb[testname].insert_many(result)
+        #print(docs)
+            
+    client.close()
--- monitor/process2.sh
+++ monitor/process2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# Example ./process2.sh sTest2 shardingtest-shard-00-00.zq5bn.mongodb.net 27016 
 TESTNAME=$1
 #SECONDARY=functionaltest-shard-00-01.zq5bn.mongodb.net
 SECONDARY=$2			# OR mongos in a sharded cluster
@@ -8,10 +8,24 @@ SECONDARY=$2			# OR mongos in a sharded cluster
 PORT=$3				# 27016 for Atlas cluster mongos
 STARTTIME=`date +"%T"`
 REPORTING_DB_URI=mongodb+srv://admin:power_low12@tradeservice.aamtz.gcp.mongodb.net
+#REPORTING_DB_URI=mongodb+srv://main_admin:bugsyBoo@m10basicagain.vmwqj.mongodb.net/gm_test
+ITERATIONS=2
+NUMROWS=100
+inc=0
+while [ $inc -le $ITERATIONS ] 
+do
+    rm -f "$TESTNAME.json"
+    mongostat --host=$SECONDARY --port=$PORT --authenticationDatabase admin --ssl  --username admin --password GreenMeadows --rowcount=$NUMROWS --all --discover --json \
+	  -O='transactions.totalCommitted,transactions.commitTypes.noShards.successful,transactions.commitTypes.singleShard.successful,transactions.commitTypes.singleWriteShard.successful,transactions.commitTypes.readOnly.successful,transactions.commitTypes.twoPhaseCommit.successful,transactions.commitTypes.recoverWithToken.successful,opcounters,extra_info.user_time_us,extra_info.system_time_us' \
+       | tee "$TESTNAME.json"
+    mongoimport --uri "$REPORTING_DB_URI" --collection $TESTNAME --type json --file "$TESTNAME.json"
+    ((inc=$inc+1))
+done
 
-mongostat --host=$SECONDARY --port=$PORT --authenticationDatabase admin --ssl  --username admin --password GreenMeadows --all --discover --json \
-	  -O='transactions.totalCommitted,transactions.commitTypes.noShards.successful,transactions.commitTypes.singleShard.successful,transactions.commitTypes.singleWriteShard.successful,transactions.commitTypes.readOnly.successful,transactions.commitTypes.twoPhaseCommit.successful,transactions.commitTypes.recoverWithToken.successful' | tee "$TESTNAME.json" | \
-    mongoimport --uri "$REPORTING_DB_URI" --db gmTests --collection $TESTNAME --type json 
 
 
+# mongoimport --uri "mongodb+srv://main_admin:bugsyBoo@m10basicagain.vmwqj.mongodb.net/gm_test" --collection sTest2 --type json
+# mongostat --uri mongodb+srv://admin:GreenMeadows@shardingtest.zq5bn.mongodb.net 
 
+#mongostat --host=$SECONDARY --port=$PORT --authenticationDatabase admin --ssl  --username admin --password GreenMeadows --rowcount=$NUMROWS --all --discover --json \
+#	  -O='transactions.totalCommitted,transactions.commitTypes.noShards.successful,transactions.commitTypes.singleShard.successful,transactions.commitTypes.singleWriteShard.successful,transactions.commitTypes.readOnly.successful,transactions.commitTypes.twoPhaseCommit.successful,transactions.commitTypes.recoverWithToken.successful,opcounters,extra_info.user_time_us,extra_info.system_time_us'
